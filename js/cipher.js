9c6d2ba384c0410e57c5afe233ce3f999db5ef73
JannikPC <jgrausen@gmail.com>
2016-03-18 13:53:27 +0100

    cypher v1.0

---
+++ js/cipher.js
@@ -0,0 +1,36 @@
+"use strict";
+
+
+/*
+ * When pirates need to cript / decrypt some text so nobody will find out.
+ * 
+ */
+function getCrypt(Decrypt) {
+	var shiftText = document.getElementById("myShifter").value;
+	if (!/^-?\d+$/.test(shiftText)) {
+		alert("Argh, matey..! put in an integer");
+		return;
+	}
+	var shift = parseInt(shiftText, 10);
+	if (shift < 0 || shift >= 26) {
+		alert("%%/#! Ahooty! what didn't you get about 0-26");
+		return;
+	}
+	if (Decrypt)
+		shift = (26 - shift) % 26;
+	var textElem = document.getElementById("myTextArea");
+	textElem.value = caesarShift(textElem.value, shift);
+}
+
+
+
+function caesarShift(text, shift) {
+	var result = "";
+	for (var item = 0; item < text.length; item++) {
+		var c = text.charCodeAt(item);
+		if      (c >= 65 && c <=  90) result += String.fromCharCode((c - 65 + shift) % 26 + 65);  // Uppercase
+		else if (c >= 97 && c <= 122) result += String.fromCharCode((c - 97 + shift) % 26 + 97);  // Lowercase
+		else                          result += text.charAt(item);  // Copy
+	}
+	return result;
+}
